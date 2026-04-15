53a3b9d72e38b601acadf9cfb37319e0232e9898
Joeliuijane <joeliuijane2@gmail.com>
2026-04-15 23:14:36 +0800

    132

--- js/homepost.js
+++ js/homepost.js
@@ -181,7 +181,7 @@ function createCard(post) {
 
   card.innerHTML = `
     <div class="hp-media">
-      <img src="${post.image}" alt="${post.title} " loading="lazy" >
+      <img src="${post.image}" alt="${post.title} " loading="lazy"  decoding="async" >
     </div>
 
     <div class="hp-body">
